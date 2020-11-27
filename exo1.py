e290a0b57cc773e8021b530070dbda9527b01083
bigotm <52489711+bigotm@users.noreply.github.com>
2020-11-27 12:12:46 +0100

    Update exo1.py

--- exo1.py
+++ exo1.py
@@ -3,12 +3,15 @@ def chiffrePortebonheur(nb):
     s=0
     for i in nb:
         s += int(i)*int(i)
-        if s > 10:
-            s += int(i) * int(i)
-    if s == 1:
-        print("Votre chiffre est porte bonheur")
-    elif(s < 10 & s>1) :
-        print("Votre chiffre n'est pas porte bonheur")
+        while s > 10:
+            for i in nb:
+                s += int(i) * int(i)
+            if s == 1:
+                print("Votre chiffre est porte bonheur")
+            elif (s < 10 & s > 1):
+                print("Votre chiffre n'est pas porte bonheur")
+        return s
+
     return s
 
 
